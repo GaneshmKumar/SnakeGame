eac3fc6b0cbf83a747c23eae5320f6f71077dd16
GaNeShKuMaRm <ganeshkumar.m1996@gmail.com>
2017-06-25 20:04:30 +0530

    removed drawCircle funtion

--- js/draw_component.js
+++ js/draw_component.js
@@ -7,10 +7,3 @@ function drawRect(x, y, width, height, color) {
 	canvasContext.fillStyle = color;
 	canvasContext.fillRect(x, y, width, height);
 }
-
-function drawCircle(x, y, radius, start, end, clockwise, color) {
-	canvas.fillStyle = color;
-	canvasContext.beginPath();
-	canvasContext.arc(x, y, radius, start, end, clockwise);
-	canvasContext.fill();
-}
